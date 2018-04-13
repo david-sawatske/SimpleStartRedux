48dd96d816d500e2c0c0912242e9ee314befca36
David Sawatske <sawatskeda10@gmail.com>
2018-04-13 17:30:53 -0500

    Simplyify store configuration

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 import rootReducer from '../reducers/root_reducer';
 
-const middleware = [thunk];
-
-if (process.env.NODE_ENV !== 'production') {
-  const { logger } = require('redux-logger');
-  middleware.push(logger);
-}
-
-const configureStore = (preloadedState = {}) => (
-  createStore(
-    rootReducer,
-    preloadedState,
-    applyMiddleware(...middleware)
-  )
+const configureStore = () => (
+  createStore(rootReducer, applyMiddleware(thunk, logger))
 );
 
 export default configureStore;
